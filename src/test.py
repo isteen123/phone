2244ec2c3b15e4ca6ee496683efac6b9845fb901
isteen123 <79262993+isteen123@users.noreply.github.com>
2021-07-02 12:53:19 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, Flask
 import sqlite3
 import os
 
-currentdirectory = (postgres://uhrljkvagsdvbp:2b4c30d656d914d2da0a7fd00f814ff90f4b929f5e118341415ed194d56d8841@ec2-34-193-112-164.compute-1.amazonaws.com:5432/dcl1cish3o860t)
-
 app = Flask(__name__)
 
 
@@ -16,11 +14,12 @@ def main():
 def phonebook():
     name = request.form["textfield"]
     phonenumber = request.form["textfield1"]
-    connection = sqlite3.connect(currentdirectory + "\phoneBook.db")
-    cursor = connection.cursor()
+#     connection = sqlite3.connect(currentdirectory + "\phoneBook.db")
+    conn = sqlite3.connect('phoneBook.db')
+    cursor = conn.cursor()
     query1 = "INSERT INTO phone VALUES('{n}','{pnm}')".format(n=name, pnm=phonenumber)
     cursor.execute(query1)
-    connection.commit()
+    conn.commit()
     return '''<script>alert("SUCCESS");window.location='/'</script>'''
 
 @app.route("/resultpage", methods=["GET"])
@@ -28,8 +27,9 @@ def resultpage():
     try:
         if request.method == "GET":
             name = request.args.get("NAME")
-            connection = sqlite3.connect(currentdirectory + "\phonebook.db")
-            cursor = connection.cursor()
+#             connection = sqlite3.connect(currentdirectory + "\phonebook.db")
+            conn = sqlite3.connect('phoneBook.db')
+            cursor = conn.cursor()
             query = "SELECT  * from phone"
             result = cursor.execute(query)
             result = result.fetchall()
